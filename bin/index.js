5c80e0bd5d8026d6e5a75fe4a1d5ee3c175ee9dc
ashe <asiasheann@gmail.com>
2022-08-03 14:52:53 +0800

    [SETUP] git init

---
+++ bin/index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk'
+import inquirer from 'inquirer'
+import { exec } from 'child_process'
+
+var prefix
+var note
+
+async function selectPrefix() {
+  const answers = await inquirer.prompt({
+    type: 'list',
+    message: 'Select git comment prefix: ',
+    name: 'prefix',
+    choices: ['[FEATURE]', '[BUGFIX]', '[SETUP]', '[STYLE]'],
+  })
+
+  prefix = answers.prefix
+  prefix === '[FEATURE]'
+    ? console.log(chalk.blue(prefix))
+    : prefix === '[BUGFIX]'
+    ? console.log(chalk.magenta(prefix))
+    : prefix === '[SETUP]'
+    ? console.log(chalk.yellowBright(prefix))
+    : console.log(chalk.cyanBright(prefix))
+}
+
+async function comment() {
+  const message = await inquirer.prompt({
+    type: 'input',
+    message: 'Write your comment: ',
+    name: 'gitComment',
+  })
+
+  note = message.gitComment
+  console.log(chalk.white(note))
+}
+
+async function handleGitCommit() {
+  var command = 'git commit -m ' + `"${prefix} ${note}"`
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`)
+      return
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+  })
+}
+
+await selectPrefix()
+await comment()
+await handleGitCommit()
